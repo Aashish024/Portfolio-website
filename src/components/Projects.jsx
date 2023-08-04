283f0f8cf6134dbf97cd6cbbc411b57b5f941635
Aashish024 <115114200+Aashish024@users.noreply.github.com>
2023-08-05 03:05:58 +0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,28 +5,37 @@ function Projects() {
   const [projects] = useState([
     {
       title: "Project 1",
-      img: "/P1-image.jpeg",
-      gLink: "https://github.com/Aashish024",
-      lLink: "https://mangtaandgallo.netlify.app/",
-      disc: `This is the first project I have created while learning about HTML and CSS from a course named "HTML and CSS in depth by meta" on Coursera. During this course I created a responsive website using flex display this is the front page of the website for a jewellerystore named MangtaandGallo`,
+      img: "/P3.jpeg",
+      gLink: "https://github.com/Aashish024/Note-App",
+      lLink: "https://github.com/Aashish024/Note-App",
+      disc: `This is an Note App, I have created this app using Flutter(dart). 
+      In this app I have connected a database using firebase so that new user can signup 
+      account or can login with an existing one, after login users can create there Notes 
+      on create Note screen by just type any input they want to add in note once user create
+      a new note, note with when it was created will be visible to there home screen 
+      and user can delete or update there note as well.`,
     },
     {
-      title: "Project 1",
-      img: "/p1-image2.jpeg",
+      title: "Project 2",
+      img: "/P4.jpeg",
+      gLink: "https://github.com/Aashish024/Order_Processing_App",
+      lLink: "https://github.com/Aashish024/Order_Processing_App",
+      disc: `This is an order processing app I have created this app using Flutter(dart).
+      In this app new user can signup new account or can login with an existing one after
+       login users can create there order on create order screen by providing details of 
+       order once order details are implemented users can see there order on home screen 
+       and can check or update there order status as well.`,
+    },
+    {
+      title: "Project 3",
+      img: "/p1-image.jpeg",
       gLink: "https://github.com/Aashish024",
       lLink: "https://mangtaandgallo.netlify.app//",
-      disc: `This is the first project I have created while learning about HTML and CSS from a course named "HTML and CSS in depth by meta" on Coursera. During this course I created a responsive website using flex display this is the front page of the website for a jewellerystore named MangtaandGallo`,
+      disc: `This is the first project I have created while learning about HTML and CSS from a course named "HTML and CSS in depth by meta" on Coursera. During this course I created a responsive website using flex display this is the front page of the website for a jewellerystore named MangtaandGallo.`,
     },
     {
-      title: "Project 2",
+      title: "Project 4",
       img: "/P2-image.png",
-      gLink: "https://github.com/Aashish024",
-      lLink: "https://littlelemonrestaurant.netlify.app/",
-      disc: `This is the second project I have created while learning about HTML and CSS from a course named "HTML and CSS in depth by meta" on Coursera. During this course I created a responsive website using grid display this is the front page of the website for a Restaurant named LittleLemon`,
-    },
-    {
-      title: "Project 2",
-      img: "/P2-image2.png",
       gLink: "https://github.com/Aashish024",
       lLink: "https://littlelemonrestaurant.netlify.app/",
       disc: `This is the second project I have created while learning about HTML and CSS from a course named "HTML and CSS in depth by meta" on Coursera. During this course I created a responsive website using grid display this is the front page of the website for a Restaurant named LittleLemon`,
